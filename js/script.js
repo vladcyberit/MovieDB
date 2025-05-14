562cb7ef95765fecd2320a73df3c79008f7000e8
Vlad <vladcyberit@gmail.com>
2025-05-14 21:50:15 +0300

    Edited some HTML elements in JS, added dynamic list of watched movies

--- js/script.js
+++ js/script.js
@@ -24,3 +24,28 @@ const movieDB = {
     ]
 };
 
+const promoAdv = document.querySelector(".promo__adv"),
+    ads = promoAdv.querySelectorAll("img"),
+    promoBg = document.querySelector(".promo__bg"),
+    promoGenre = promoBg.querySelector(".promo__genre"),
+    promoList = document.querySelector(".promo__interactive-list");
+
+ads.forEach(item => {
+    item.remove();
+});
+
+promoGenre.textContent = "ДРАМА";
+
+promoBg.style.background = "url('img/bg.jpg') center top / cover no-repeat";
+
+promoList.innerHTML = "";
+
+const sortedMovies = movieDB.movies.slice().sort();
+
+sortedMovies.forEach((item, i) => {
+    promoList.insertAdjacentHTML("beforeend", `
+        <li class="promo__interactive-item">${i + 1}. ${item}
+            <div class="delete"></div>
+        </li>
+    `);
+});
